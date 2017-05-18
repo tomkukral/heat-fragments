9a521529fa76bc4421869fc7daa439980b68417e
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-05-18 22:42:17 +0200

    fix cfn-signal

--- script/instance_boot.sh
+++ script/instance_boot.sh
@@ -33,7 +33,7 @@ function wait_condition_send() {
 	else
 		aws_status="false"
 	fi
-	cnf-signal -s "$aws_status" --resource "$aws_resource" --stack "$aws_stack" --region "$aws_region"
+	cfn-signal -s "$aws_status" --resource "$aws_resource" --stack "$aws_stack" --region "$aws_region"
   else
   	# Heat
   	$wait_condition_notify -k --data-binary "$data_binary"
